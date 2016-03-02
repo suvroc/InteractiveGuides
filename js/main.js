3226ecf7cfe7d0ba1cbdb2dc2880404d9176ceeb
Adrian <suvroc@o2.pl>
2016-03-02 19:26:03 +0100

    Update 2016-03-02T18:25:52.086Z

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $('.example a').click(function(event) {
-    var selector = $(event.target).closests('a').attr('href');
+    var selector = $(event.target).closest('a').attr('href');
     
     $('.guides .guide').hide(0, function() {
         $(selector.replace('.', '\\.')).show(0);
