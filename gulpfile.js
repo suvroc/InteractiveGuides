c41d81084af88c903acc3f2c635ca4f88966023d
Adrian <suvroc@o2.pl>
2016-03-02 18:52:52 +0100

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,9 @@ gulp.task('deploy', function () {
   return gulp.src("./dist/**/*")
     .pipe(deploy())
 });
+
+gulp.task('copy', function() {
+    gulp.src('./src/**/*')
+    // Perform minification tasks, etc here
+    .pipe(gulp.dest('./dist'));
+});
